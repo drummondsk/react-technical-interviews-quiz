20ba545eb5301ac9374b74b83cb6ee9af99ddc77
Kyle Drummonds <89232875+drummondsk@users.noreply.github.com>
2023-10-08 12:31:55 -0400

    Update Questions.tsx

---
+++ src/components/Questions.tsx
@@ -0,0 +1,15 @@
+import { Question } from "../types"
+import Answers from "./Answers";
+import Questions_module from './Questions.module.scss';
+type Props = {
+    question: Question;
+    onSubmit: (correct: boolean) => void
+}
+
+function QuesitonComp(props: Props) {
+    return <div>
+        <h3 className={Questions_module.question}>{props.question.question}</h3>
+        <Answers question={props.question} onSubmit={props.onSubmit} />
+    </div>
+}
+export default QuesitonComp
